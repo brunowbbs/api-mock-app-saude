a0c26186e20efc02b8911cc2ab44fe94d50f9c9f
Wesley Bruno <engwesleybruno@gmail.com>
2023-07-01 20:57:23 -0300

    feat: register

--- index.js
+++ index.js
@@ -80,18 +80,14 @@ server.post("/register", async (req, res) => {
       .json({ message: "Email, name, phone and password values are required" });
   }
 
-  if (email === "admin@admin.com" && password === "123456") {
-    return res.status(200).json({
-      name: name,
-      email: email,
-      token,
-      phone: "+55 21 99999-9999",
-      picture:
-        "https://igd-wp-uploads-pluginaws.s3.amazonaws.com/wp-content/uploads/2016/05/30105213/Qual-e%CC%81-o-Perfil-do-Empreendedor.jpg",
-    });
-  }
-
-  return res.status(400).json({ message: "Invalid email or password" });
+  return res.status(200).json({
+    name: name,
+    email: email,
+    token,
+    phone: "+55 21 99999-9999",
+    picture:
+      "https://igd-wp-uploads-pluginaws.s3.amazonaws.com/wp-content/uploads/2016/05/30105213/Qual-e%CC%81-o-Perfil-do-Empreendedor.jpg",
+  });
 });
 
 server.get("/activities", authorization, async (req, res) => {
